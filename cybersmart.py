f03e21c718d8befd7cce9290c6fb8f51cc622f20
Ibrahim <ishelukindo4@gmail.com>
2025-05-27 18:58:04 +0530

    Fourth Commit

--- cybersmart.py
+++ cybersmart.py
@@ -216,4 +216,49 @@ driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 time.sleep(3)
 
 
+# Public Internet Section
+driver.find_element(By.XPATH, "//button[normalize-space()='Next']").click()
+time.sleep(3)
 
+#Video
+wait = WebDriverWait(driver, 15)
+video = wait.until(EC.presence_of_element_located((By.XPATH, "//video[@class='video shadow']")))
+driver.execute_script("arguments[0].play();", video)
+time.sleep(60)
+
+#Take quiz
+wait = WebDriverWait(driver, 20)
+take_quiz_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Take Quiz']")))
+take_quiz_button.click()
+time.sleep(2)
+
+# Questions
+Qst1= driver.find_element(By.LINK_TEXT, "Both")
+Qst1.click()
+time.sleep(2)
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click() 
+time.sleep(3)
+
+Qst2= driver.find_element(By.XPATH, "//section[@class='main-conrainer scrolly']//li[1]//a[1]") 
+Qst2.click()
+time.sleep(3)
+driver.find_element(By.XPATH, "//button[normalize-space()='Next']").click()
+time.sleep(3)
+
+Qst3= driver.find_element(By.LINK_TEXT, "True")
+Qst3.click()
+time.sleep(3)
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+time.sleep(3)
+
+Qst4= driver.find_element(By.XPATH, "//section[@class='main-conrainer scrolly']//li[2]//a[1]")
+Qst4.click()
+time.sleep(3)
+driver.find_element(By.XPATH, "//button[normalize-space()='Next']").click()
+time.sleep(3)
+
+Qst5= driver.find_element(By.XPATH, "//span[normalize-space()='False']")
+Qst5.click()
+time.sleep(3)
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+time.sleep(3)
