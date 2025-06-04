97426828646de9ccd16aeb4ed7c2ef18000a8005
Ibrahim <ishelukindo4@gmail.com>
2025-06-04 23:39:01 +0530

    Sixth Commit

--- cybersmart.py
+++ cybersmart.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import os
 import time
