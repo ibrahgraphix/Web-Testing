5b6ada45018ad16a3d7379fb081756fc8b118eb7
Ibrahim <ishelukindo4@gmail.com>
2025-06-04 22:54:25 +0530

    Fifth Commit

--- cybersmart.py
+++ cybersmart.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import os
 import time
 
 #Error handling code
@@ -34,7 +37,19 @@ def safe_select_by_text(driver, by, value, visible_text, description="", wait_ti
         
 
 #Main Code start here..
-driver= webdriver.Chrome()
+
+# Set up download directory
+download_dir = os.path.abspath("downloads")  # Create 'downloads' folder in your project
+
+chrome_options = Options()
+chrome_options.add_experimental_option('prefs', {
+    "download.default_directory": download_dir,
+    "plugins.always_open_pdf_externally": True,  # Auto-download PDFs
+    "download.prompt_for_download": False,
+    "profile.default_content_settings.popups": 0,
+})
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.maximize_window()
 driver.get("https://cybersmart.wnscaresfoundation.org/Students")
 
@@ -271,7 +286,7 @@ time.sleep(3)
 wait = WebDriverWait(driver, 15)
 video = wait.until(EC.presence_of_element_located((By.XPATH, "//video[@class='video shadow']")))
 driver.execute_script("arguments[0].play();", video)
-time.sleep(60)
+time.sleep(110)
 
 #Take quiz
 wait = WebDriverWait(driver, 20)
@@ -319,3 +334,13 @@ checker = WebDriverWait(driver, 20)
 take_quiz_button = checker.until(EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Download']")))
 take_quiz_button.click()
 time.sleep(2)
+
+try:
+    pdf_link = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//a[contains(@href, ".pdf")]'))
+    )
+    pdf_link.click()
+    print("✅ PDF link clicked successfully.")
+except:
+    print("⚠️ PDF link not found or not clickable.")
+
